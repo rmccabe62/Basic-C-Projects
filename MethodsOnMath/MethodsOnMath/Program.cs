a52a846c8ce6bef2c66613144a66ac520f616aff
rmccabe62 <rmccabe1023@gmail.com>
2021-02-25 14:34:15 -0500

    completed methods on math assignment

---
+++ MethodsOnMath/MethodsOnMath/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsOnMath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number to be used in arithmatic operations.");
+            int numChoice = Convert.ToInt32(Console.ReadLine());
+            mathOperations math = new mathOperations();
+            Console.WriteLine("Your number when added to another number is: ");
+            Console.WriteLine(math.AddNumber(numChoice));
+
+            Console.WriteLine("Your number when subtracted from another number is: ");
+            Console.WriteLine(math.SubtNumber(numChoice));
+
+            Console.WriteLine("Your number when multipled by another number is: ");
+            Console.WriteLine(math.multNumber(numChoice));
+            Console.ReadLine();
+        }
+       
+    }
+}
---
+++ MethodsOnMath/MethodsOnMath/mathOperations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsOnMath
+{
+    public class mathOperations
+    {
+        public  int AddNumber(int number)
+        {
+            
+                
+            int sum = number + 7;
+            return sum;
+        }
+        public int SubtNumber(int number2)
+        {
+            int difference = number2 - 2;
+            return difference;
+        }
+        public int multNumber(int number3)
+        {
+            int product = number3 * 8;
+            return product;
+        }
+    }
+}
