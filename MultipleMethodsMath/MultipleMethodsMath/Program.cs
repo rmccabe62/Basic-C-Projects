346d062339016bffd2f02899db66f9c701480282
rmccabe62 <rmccabe1023@gmail.com>
2021-02-26 11:10:41 -0500

    submit for review

---
+++ MultipleMethodsMath/MultipleMethodsMath/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleMethodsMath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a whole number that will be used to perform some calculations.");
+            int numChoice = Convert.ToInt32(Console.ReadLine());
+            mathOverload add = new mathOverload();
+            Console.WriteLine("Your number when added to another number is equal to: ");
+            Console.WriteLine(add.mathOperations(numChoice));
+
+            mathOverload subtract = new mathOverload();
+            Console.WriteLine("Another number subtracted from your number is: ");
+            Console.WriteLine(subtract.mathOperations(numChoice));
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ MultipleMethodsMath/MultipleMethodsMath/mathOverload.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleMethodsMath
+{
+    class mathOverload
+    {
+        public int mathOperations(int num1)
+        {
+            int sum = num1 + 10;
+            return sum;
+        }
+        public int mathOperations(int num1, double num2)
+        {
+            double difference = num2 - 2;
+            int newNum = Convert.ToInt32(difference);
+            return newNum;
+        }
+    }
+}
