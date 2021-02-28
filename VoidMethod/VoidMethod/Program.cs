adf44f19115c7dac321ce777198f184bdfdbef15
rmccabe62 <rmccabe1023@gmail.com>
2021-02-28 14:59:19 -0500

    completed void method

---
+++ VoidMethod/VoidMethod/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            voidTwoNums mathDisplay = new voidTwoNums();
+            Console.WriteLine("Please enter a number to be added to another number. ");
+            int enterNum = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Please enter a second number to be displayed to the screen.");
+            int secNum =Convert.ToInt32(Console.ReadLine());
+            mathDisplay.voidNumbers(enterNum, secNum);
+            Console.ReadLine();
+            
+            
+        }
+    }
+}
---
+++ VoidMethod/VoidMethod/voidTwoNums.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class voidTwoNums
+    {
+        public void voidNumbers(int num1, int num2)
+        {
+            int sum = num1 + num2;
+            Console.WriteLine(num2);
+            
+        }
+    }
+}
