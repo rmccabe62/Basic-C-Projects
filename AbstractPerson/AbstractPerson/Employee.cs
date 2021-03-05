a238d7659bbc8bbc9367060e395e000c22ff3180
rmccabe62 <rmccabe1023@gmail.com>
2021-03-05 18:20:55 -0500

    completed parameters assignment

--- AbstractPerson/AbstractPerson/Employee.cs
+++ AbstractPerson/AbstractPerson/Employee.cs
@@ -8,9 +8,9 @@ namespace AbstractPerson
 {
     public class Employee<T> : Person, IQuittable
     {
-        public List<string> Things{ get; set; } 
+         
 
-        public List<T> id { get; set; }
+        public List<T> Things { get; set; }
         public override void SayName()
         {
             Console.WriteLine("Name: " + firstName + " " + lastName);
@@ -23,16 +23,16 @@ namespace AbstractPerson
             Console.WriteLine("You chose: " + answer);
            
         }
-        public static bool operator == (Employee name1, Employee name2) //establish a comparisiom to see if two employee objects have identicvcal id's
-        {
-            bool same = (name1.id == name2.id);
-            return (same);
-        }
-        public static bool operator !=(Employee name1, Employee name2) //compare two employee objects to check if they are not identical
-        {
-            bool same = (name1.id != name2.id);
-            return (same);
-        }
+        //public static bool operator == (Employee name1, Employee name2) //establish a comparisiom to see if two employee objects have identicvcal id's
+        //{
+        //    bool same = (name1.id == name2.id);
+        //    return (same);
+        //}
+        //public static bool operator !=(Employee name1, Employee name2) //compare two employee objects to check if they are not identical
+        //{
+        //    bool same = (name1.id != name2.id);
+        //    return (same);
+        //}
 
 
 
--- AbstractPerson/AbstractPerson/Program.cs
+++ AbstractPerson/AbstractPerson/Program.cs
@@ -10,14 +10,24 @@ namespace AbstractPerson
     {
         static void Main(string[] args)
         {
-            List<Things>
+            
            //Create a new object and display the result of a comparision to the screen
-            Employee employee = new Employee();
-            employee.id =231;
-            Employee employee2 = new Employee();
-            employee2.id =231;
-            Console.WriteLine(employee == employee2);
-            Console.WriteLine(employee != employee2);
+            Employee<string> employee = new Employee<string>();
+            employee.Things = new List<string>() { "apple", "cherry", "grapes" };
+            Employee<int> employee1 = new Employee<int>();
+            employee1.Things = new List<int>() { 2, 4, 6, 8 };
+            foreach (string fruit in employee.Things)
+            {
+                Console.WriteLine(fruit);
+            }
+            foreach (int num in employee1.Things)
+            {
+                Console.WriteLine(num);
+            }
+            //Employee employee2 = new Employee();
+            //employee2.id =231;
+            //Console.WriteLine(employee == employee2);
+            //Console.WriteLine(employee != employee2);
             Console.ReadLine();
 
         }
