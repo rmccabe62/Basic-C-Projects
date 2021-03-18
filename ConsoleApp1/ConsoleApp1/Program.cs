21334925c8a44637998270b31cf3cfc0ec470455
rmccabe62 <rmccabe1023@gmail.com>
2021-03-18 14:29:42 -0400

    updated assignment

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -16,12 +16,10 @@ namespace ConsoleApp1
             //Get the number from the user
             Console.WriteLine("Please enter a whole number.");
             string answer = Console.ReadLine();
-            //Append the number chosen to the log file
-            using (StreamWriter file = new StreamWriter(@"C:\Users\rmcca\OneDrive\Desktop\Logs\log.txt", true))
-            {
-                file.WriteLine(DateTime.Now);
-                file.WriteLine(answer);
-            }
+            int x = Convert.ToInt32(answer);
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine(x);
+            Console.ReadLine();
         }
     }
 }
