51e8e05ae201af4bea5355a8541b0470016fc572
rmccabe62 <rmccabe1023@gmail.com>
2021-03-18 15:03:43 -0400

    corrected assignment

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -15,10 +15,12 @@ namespace ConsoleApp1
             Console.WriteLine(DateTime.Now);
             //Get the number from the user
             Console.WriteLine("Please enter a whole number.");
-            string answer = Console.ReadLine();
-            int x = Convert.ToInt32(answer);
-            Console.WriteLine(DateTime.Now);
-            Console.WriteLine(x);
+            int answer = Convert.ToInt32(Console.ReadLine());
+            DateTime currentTime = DateTime.Now;
+            DateTime xHoursLater = currentTime.AddHours(answer);
+            //For reference
+            Console.WriteLine("In " + answer  + " hours it will be " + xHoursLater);
+            Console.WriteLine("In {0} hours it will be {1} ", answer, xHoursLater);
             Console.ReadLine();
         }
     }
